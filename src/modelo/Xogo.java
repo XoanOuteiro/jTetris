b81a6c67bd09f0a4986aab0a114be9eb5a37a6b7
XoanOuteiro <xoanouteirojorge@gmail.com>
2023-02-09 12:41:36 +0100

    Added non-collision additions to floor and real piece instancing

--- src/modelo/Xogo.java
+++ src/modelo/Xogo.java
@@ -3,105 +3,174 @@ package modelo;
 import iu.VentanaPrincipal;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Random;
 import javax.swing.JPanel;
 
 /**
  *
  * by: @XoanOuteiro & @Samuyo
  */
-public class Xogo{
-    
+public class Xogo {
+
     public static final int LADO_CADRADO = 20;
     public static final int MAX_X = 220;            //Changed to -20 so that X and Y detection would work
     public static final int MAX_Y = 580;
     public boolean pausa;
     public VentanaPrincipal ventanaPrincipal;
+    public JPanel holder;
     public int numeroLineas;
-    public int rowAM ;
-    public int colAM ;
-    public int celSiz ;//= to lado¿
-    public ArrayList <Cadrado> suelo;
-    public Iterator <Cadrado> it;
-    
+    public int rowAM;
+    public int colAM;
+    public int celSiz;//= to lado¿
+    public ArrayList<Cadrado> floor;
+    public Iterator<Cadrado> it;
+
     public Ficha fichaActual;
-            
+
     //Construction
-    public Xogo(JPanel holder,VentanaPrincipal ventana,int colAm){
+    public Xogo(JPanel holder, VentanaPrincipal ventana, int colAm) {
         //Match styles
         this.ventanaPrincipal = ventana;
-        
+        this.holder = holder;
+
         //Suelo
-        this.suelo = new ArrayList();
-        this.it = suelo.iterator();
-        
+        this.floor = new ArrayList();
+        this.it = floor.iterator();
+
         //TEST
-        this.fichaActual = new FichaT(120,60);
-        for (int i = 0; i < 4; i++) {
-            holder.add(fichaActual.cadrados[i].label_cadrado);
-        }
+        this.createNewRandomFicha();
     }
-    
+
     //Getters & Setters
     public Ficha getFichaActual() {
         return fichaActual;
     }
-    
+
     //Mover
-    public void moverFichaDereita(){
-        if(ePosicionValida(fichaActual.getMostRightSquare().getX() + LADO_CADRADO , fichaActual.getMostRightSquare().getY())){
+    public void moverFichaDereita() {
+        if (ePosicionValida(fichaActual.getMostRightSquare().getX() + LADO_CADRADO, fichaActual.getMostRightSquare().getY())) {
             fichaActual.moverDereita();
         }
-        
+
     }
-    
-    public void moverFichaEsquerda(){
-        if(ePosicionValida(fichaActual.getMostLeftSquare().getX() - LADO_CADRADO , fichaActual.getMostLeftSquare().getY())){
+
+    public void moverFichaEsquerda() {
+        if (ePosicionValida(fichaActual.getMostLeftSquare().getX() - LADO_CADRADO, fichaActual.getMostLeftSquare().getY())) {
             fichaActual.moverEsquerda();
         }
     }
-    
-    public void moverFichaAbaixo(){
-        if(ePosicionValida(fichaActual.getMostDownSquare().getX() , fichaActual.getMostDownSquare().getY() + LADO_CADRADO))
+
+    public void moverFichaAbaixo() {
+        if (ePosicionValida(fichaActual.getMostDownSquare().getX(), fichaActual.getMostDownSquare().getY() + LADO_CADRADO)) {
             fichaActual.moverAbaixo();
+        }
+
+        if (fichaActual.isCollideWithLowerLimit()) {
+            /*
+            * Each time the piece is moved down (always using this method by user or and timer)
+            * the routine is called and asks if the piece has reached the lower limit, if it has the
+            * consequent method is called fusing the pieces squares to the floor and then killing its entity
+             */
+            this.fuseCurrentToFloor();
+        }
     }
-    
-    public void rotarFicha(){
+
+    public void rotarFicha() {
         fichaActual.rotar();
-    } 
-    
+    }
+
     //Utility logic
     /**
-     * 
+     *
      * @param x
      * @param y
      * @return boolean
-     * 
-     * Requests a two values of a square (post-transform), if said
-     * position is not out of bounds or already occupied it returns "true"
-     * as in "this position is valid", else it returns false.
-     * 
+     *
+     * Requests a two values of a square (post-transform), if said position is
+     * not out of bounds or already occupied it returns "true" as in "this
+     * position is valid", else it returns false.
+     *
      * This method is used for all transform methods
      */
-    public boolean ePosicionValida(int x, int y){
+    public boolean ePosicionValida(int x, int y) {
         //Attributes
         boolean clampedOnX = false;
         boolean clampedOnY = false;
         boolean unOccupied = true;  //Change when collisions are made
-        
+
         //Clamped on X?
-        if( (x >= 0) && (x <= MAX_X) ){ clampedOnX = true; }
-        
+        if ((x >= 0) && (x <= MAX_X)) {
+            clampedOnX = true;
+        }
+
         //Clamped on Y?
-        if( (y >= 0) && (y <= MAX_Y) ){ clampedOnY = true; }
-        
+        if ((y >= 0) && (y <= MAX_Y)) {
+            clampedOnY = true;
+        }
+
         //Is occupied?
         //!!!TBI!!!
-        
         //Final operand
-        if(clampedOnX && clampedOnY && unOccupied){
+        if (clampedOnX && clampedOnY && unOccupied) {
             return true;
         } else {
             return false;
+        }
+    }
+
+    //Floor methods
+    public void fuseCurrentToFloor() {
+        for (int i = 0; i < 4; i++) {
+            this.floor.add(this.fichaActual.cadrados[i]);
+        }
+
+        this.createNewRandomFicha();
+    }
+
+    //Killer methods and restarts
+    public void createNewRandomFicha() {
+         Random rand = new Random();
+        int index = rand.nextInt(4) + 1;
+
+        switch (index) {
+            case 1:
+
+                this.fichaActual = new FichaT(120, 20);
+                for (int i = 0; i < 4; i++) {
+                    this.holder.add(fichaActual.cadrados[i].label_cadrado);
+                }
+
+                break;
+
+            case 2:
+
+                this.fichaActual = new FichaBarra(120, 20);
+                for (int i = 0; i < 4; i++) {
+                    this.holder.add(fichaActual.cadrados[i].label_cadrado);
+                }
+
+                break;
+
+            case 3:
+
+                this.fichaActual = new FichaCadrada(120, 20);
+                for (int i = 0; i < 4; i++) {
+                    this.holder.add(fichaActual.cadrados[i].label_cadrado);
+                }
+
+                break;
+
+            case 4:
+
+                this.fichaActual = new FichaL(120, 20);
+                for (int i = 0; i < 4; i++) {
+                    this.holder.add(fichaActual.cadrados[i].label_cadrado);
+                }
+
+                break;
+
+            default:
+                break;
         }
     }
 }
